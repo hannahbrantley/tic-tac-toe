911882de65149b504de4502b0587260c162df73d
Hannah Brantley <hannahbrantley@Hannahs-MacBook-Pro.local>
2020-05-02 12:32:32 -0600

    slightly updated version in case i need to restore

--- js/main.js
+++ js/main.js
@@ -10,8 +10,11 @@ let turn;     // 1 or -1 (player)
 let winner;   // 1 = Player 1; -1 = Player 2; 'T' = tie; null = no winner/tie
 
 /*----- cached element references -----*/
-const divEls = [...document.querySelectorAll('#board > div')];
-console.log(divEls);
+const divEls = [
+    [...document.querySelectorAll('.col0')],
+    [...document.querySelectorAll('.col1')],
+    [...document.querySelectorAll('.col2')],
+];
 const msgEl = document.getElementById('msg');
 
 /*----- event listeners -----*/
@@ -25,7 +28,11 @@ document.querySelector('button')
 init();
 
 function init() {
-  board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  board = [
+      [0, 0, 0], // Column 0
+      [0, 0, 0], // Column 1
+      [0, 0, 0] // Column 2
+  ];
   //okay maybe it has to be array of arrays so 3, 4, 5 doesn't = winner 
   turn = -1;
   winner = null;
@@ -33,30 +40,45 @@ function init() {
 }
 
 function handleClick(event) {
-  const clickedSpot = divEls.indexOf(event.target); 
-  getWinner();
-  if (board[clickedSpot] === 0){
-  board[clickedSpot] = turn;
-  console.log(board);
-  console.log(clickedSpot);
-  event.target.innerText = playerLookup[turn];
-   turn *= -1;
-} 
-
-function getWinner() {
-  for (let i = clickedSpot; i <= board.length; i++) {
-    if (Math.abs(board[i] + board[i + 1] + board[i + 2]) === 3) {
-        console.log('winner');
-    }
-  }  
+  const colIdx = event.target.className.slice(3);
+  const rowIdx = event.target.id.slice(3); 
+  let clickedSpot = board[colIdx][rowIdx];
+//   console.log(event.target);
+//   console.log(colIdx);
+//   console.log(rowIdx);
+//   console.log(clickedSpot);
+//   getWinner(colIdx, rowIdx);
+ if (clickedSpot === 0){
+    board[colIdx][rowIdx] += turn;
+    console.log(board);
+    console.log(clickedSpot);
+    event.target.innerText = playerLookup[turn];
+    turn *= -1;
 }
+console.log(board);
+};
+
+// function getWinner(colIdx, rowIdx) {
+//   for (let i = 0; i <= board.length; i++) {
+//     checkUp();
+//     // checkAcross();
+//     // checkDiag();
+//   }  
+// }
+
+// function checkUp(colIdx, rowIdx) {
+//     if (Math.abs(board[colIdx][rowIdx] + board[colIdx][rowIdx + 1] + board[colIdx][rowIdx + 2]) === 3) {
+//         return 'winner';
+//       } else {
+//         return null;
+//       }
+// }
+
+render();
 
 
-}
 
 function render(){
     console.log('rendered');
     msgEl.innerHTML = `<span style="color: ${playerLookup[turn]}">${playerLookup[turn].toUpperCase()}'s</span> Turn`;
-}
-
-
+};
